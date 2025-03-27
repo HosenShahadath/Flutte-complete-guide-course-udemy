7528d4ba7a9af548b73ced5bfca0ccca5c40d349
HosenShahadath <dev.shahadathhosen@gmail.com>
2025-03-27 08:04:27 +0600

    hello world

--- lib/02 Flutter & Dart Basic I/hello_world.dart
+++ lib/02 Flutter & Dart Basic I/hello_world.dart
@@ -7,7 +7,24 @@ class HelloWorld extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('First App')),
-      body: Center(child: Text('Hello World')),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 26, 2, 80),
+              Color.fromARGB(255, 45, 7, 98),
+            ],
+          ),
+        ),
+        child: const Center(
+          child: Text(
+            'Hello World',
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+        ),
+      ),
     );
   }
 }
